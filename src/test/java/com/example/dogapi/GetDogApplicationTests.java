447ec0050114452b5e591d2e0188916424ed7aa5
llHelltakerll <llhelltakerll@gmail.com>
2024-04-21 22:03:33 +0300

    tests added

--- src/test/java/com/example/dogapi/GetDogApplicationTests.java
+++ src/test/java/com/example/dogapi/GetDogApplicationTests.java
@@ -1,13 +1,8 @@
 package com.example.dogapi;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GetDogApplicationTests {
 
-    @Test
-    void contextLoads() {
-
-    }
 }
