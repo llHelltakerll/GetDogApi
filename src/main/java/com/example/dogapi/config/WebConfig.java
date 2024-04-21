dd231b50de18f663c68b808a843f3cc2e00e2104
llHelltakerll <llhelltakerll@gmail.com>
2024-04-22 02:36:34 +0300

    tests added

--- src/main/java/com/example/dogapi/config/WebConfig.java
+++ src/main/java/com/example/dogapi/config/WebConfig.java
@@ -1,4 +1,4 @@
-package config;
+package com.example.dogapi.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
